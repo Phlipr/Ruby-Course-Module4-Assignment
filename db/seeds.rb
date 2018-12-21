401c6ba9880dec068089a48d92948897eb0a9b78
Phlipr <preynolds1406@gmail.com>
2018-12-21 10:40:40 -0600

    Updated spec, db, and Gemfiles for new assignment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Destroy all current data in databases
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
+
 User.destroy_all
 TodoList.destroy_all
-TodoItem.destroy_all
-Profile.destroy_all
 
-# Setup Users
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
 
-User.create! [
-  { username: "Fiorina", password_digest: "RunningForSenate" },
-  { username: "Trump", password_digest: "MAGAt" },
-  { username: "Carson", password_digest: "IWishIWerePresident" },
-  { username: "Clinton", password_digest: "IAmSupposedToBePresident" }
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
+end
+
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
 ]
 
-
-# Setup Profiles
-
-carly = User.find_by username: "Fiorina"
-
-carly.create_profile gender: "female", birth_year: 1954, first_name: "Carly", last_name: "Fiorina"
-
-donald = User.find_by username: "Trump"
-
-donald.create_profile gender: "male", birth_year: 1946, first_name: "Donald", last_name: "Trump"
-
-ben = User.find_by username: "Carson"
-
-ben.create_profile gender: "male", birth_year: 1951, first_name: "Ben", last_name: "Carson"
-
-hillary = User.find_by username: "Clinton"
-
-hillary.create_profile gender: "female", birth_year: 1947, first_name: "Hillary", last_name: "Clinton"
-
-
-# Setup todo_lists
-
-User.all.each {
-  |user| user.todo_lists.create! [ { list_name: "To Do List for #{user.profile.first_name}:", list_due_date: (Date.today + 1.year) } ]
-}
-
-# Setup todo_items
-
-TodoList.all.each {
-  |list|
-    person = User.find(list.user_id)
-    name = person.profile.first_name
-    for i in 0..4 do
-      list.todo_items.create! due_date: (Date.today + 1.year), title: "Item #{ ( i + 1 ) } for #{name}", description: "This is item #{ ( i + 1 ) } on the todo list for #{name}.", completed: false
-    end
-}
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
+end
